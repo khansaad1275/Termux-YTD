5591842a46d2f18ff3bfc5077bfd71481e8e0baf
khan saad <32749921+khansaad1275@users.noreply.github.com>
2020-04-14 01:33:11 +0530

    Create Install.sh

---
+++ Install.sh
@@ -0,0 +1,37 @@
+#!/data/data/com.termux/files/usr/bin/bash
+# Project name : Termux-YTD
+# Coded by: Khansaad1275 (You dont become a coder by just changing the credits)
+# Github: https://github.com/khansaad1275/Termux-YTD
+# Date : 14/4/2020
+
+echo -e "\e[035m"  "Updating default packages\n"
+apt update && apt upgrade -y
+
+echo -e "\e[032m" "Requesting acces to storage\n"
+sleep 2
+echo -e "\e[032m" "Allow Storage Permission!"
+sleep 2
+termux-setup-storage
+sleep 5
+
+echo -e "\e[033m" "Installing python\n"
+pkg install python -y
+
+echo -e "\e[034m"  "Installing youtube-dl\n"
+pip install youtube-dl
+
+echo -e "\e[032m"  "Making thw Youtube Directory to download the Vidoes\n"
+mkdir ~/storage/shared/Youtube
+
+echo -e "\e[036m"  "Creating youtube-dl folder for config\n"
+mkdir -p ~/.config/youtube-dl
+
+echo -e "Creating bin folder\n"
+mkdir ~/bin
+
+echo -e "Creating Termux-URL-Opener Script.\n"
+
+mv termux-url-opener.bs ~/bin/termux-url-opener
+
+echo -e "\n"
+echo -e "\e[032m" "Process Complete!"
