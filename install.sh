af13c6759ccb90ce1852bfde236e65e4043f1fa5
Oak atsume <56260438+user951@users.noreply.github.com>
2020-12-02 16:26:21 -0800

    Update install.sh
    Install.sh update

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
 #!/data/data/com.termux/files/usr/bin/bash
+
 # Project name : Termux-YTD
 # Coded by: Khansaad1275 (You dont become a coder by just changing the credits)
 # Github: https://github.com/khansaad1275/Termux-YTD
 # Date : 14/4/2020
 
+#Probably make a varible with the file name 
+#make it more easy to update!
 echo -e "\e[035m"  "Updating default packages\n"
 apt update && apt upgrade -y
 
@@ -11,7 +14,7 @@ echo -e "\e[032m" "Requesting acces to storage\n"
 sleep 2
 echo -e "\e[032m" "Allow Storage Permission!"
 sleep 2
-termux-setup-storage
+termux-setup-storage 
 sleep 5
 
 echo -e "\e[033m" "Installing python\n"
@@ -32,6 +35,9 @@ mkdir ~/bin
 echo -e "Creating Termux-URL-Opener Script.\n"
 
 mv termux-url-opener ~/bin/
+#Oh hey Don't forget to chmod that file there!
+
+#chmod +x ~/bin/termux-url-opener
 echo -e "\n"
 echo -e "\e[032m" "Process Complete!"
 echo -e "\e[032m" "Now you can share any Youtube video with Termux and you will be ask to select the quality of your downloaded video and after that,It will be automatically Downloaded"
