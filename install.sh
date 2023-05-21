5a6e69a50364fb83a7c8a108a0e1eecfeaeb68bc
Mantu kumar <66949105+Mantukumar007@users.noreply.github.com>
2023-05-21 07:08:52 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,4 @@
-#!/data/data/com.termux/files/usr/bin/bash
 
-# Project name : Termux-YTD
-# Coded by: Khansaad1275 (You dont become a coder by just changing the credits)
-# Github: https://github.com/khansaad1275/Termux-YTD
-# Date : 14/4/2020
 
 TERMUX_HOME="/data/data/com.termux/files/home"
 
